204a0d87cf8d32771377a0454e46e532bf20a1fc
jbarry89 <joelbarry8907@hotmail.com>
2025-02-23 23:39:03 -0500

    Modified code

--- src/config/connection.ts
+++ src/config/connection.ts
@@ -2,14 +2,11 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/socialnetwork', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
+        await mongoose.connect('mongodb://localhost:27017/socialnetwork');
         console.log('MongoDB is Connected!');
     } catch (error) {
         console.error('MongoDB connection Failed.', error);
-        process.exit(1);  // Exit the process when connection failed/.
+        process.exit(1);  // Exit the process when connection failed.
     }
 
 };
